973b7fbbb6e415c858a3708f0137a7c68c5528d7
Diego <diego.gerardo95@hotmail.com>
2024-09-22 22:01:16 -0300

    Add mysql database connection & minor changes to routers

--- app/api/models/user_model.py
+++ app/api/models/user_model.py
@@ -1,1 +1,13 @@
-from sqlalchemy import Column, Integer, String 
+from sqlalchemy import Column, Integer, String, BINARY
+from dependencies.database import base
+
+class Users(base):
+    __tablename__ = 'Users'
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, index=True, unique=True)
+    hashed_pass = Column(BINARY)
+    salt = Column(BINARY)
+
+
+    
--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from api.schemas.user_schema import user_schema_create
+from api.dependencies.database import get_db
+from sqlalchemy.orm import Session
+from app.api.models.user_model import Users
+
 user_router = APIRouter()
 
 
 @user_router.get('/{i}', tags=['Users']) #Tag sirve para ser claro en la documentacion
-async def get_user_by_id(i:int):
-
-    # Realizar conexion con base de datos con sqlalchemy.
-
-    return i
+async def get_user_by_id(i:int, db: Session = Depends(get_db)):
+ 
+    db_user: Users = db.query(Users).filter(Users.id == i).first()
+    return db_user.user_name
 
 
 @user_router.post('/', tags=['Users'])
